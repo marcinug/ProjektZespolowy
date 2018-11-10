90eb7b341d6e36b10f99199bd94c65162f1a3300
Jakub Lemka <lemka.jakub@gmail.com>
2018-11-10 11:19:57 +0100

    add main index and root sagas and reducers

--- radosnysenior/src/index.js
+++ radosnysenior/src/index.js
@@ -1,12 +1,43 @@
+import 'babel-polyfill';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import {
+  Route,
+  Router,
+  Redirect,
+} from 'react-router';
+import createSagaMiddleware from 'redux-saga';
+import reducers from './state/reducers';
+import sagas from './state/sagas';
+import Example from './containers/example/example'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware)
+)
+
+sagas.forEach(sagaMiddleware.run);
+
+sagas.forEach(sagaMiddleware.run);
+
+render(
+  <Provider store={store}>
+    <Example />
+  </Provider>,
+  document.getElementById('root')
+)
+// render(
+//   <Provider store={store}>
+//     <Router history={history}>
+//       <Route path="/" component={example}>
+//         <Route path="change_password" component={example} />
+//         <Redirect from="*" to="/" />
+//       </Route>
+//     </Router>
+//   </Provider>,
+//   document.getElementById('root')
+// );
