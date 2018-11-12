fc393b5a4382e9645272d08b34b5ea533728ef00
Jakub Lemka <lemka.jakub@gmail.com>
2018-11-12 11:29:46 +0100

    react-router connected

--- radosnysenior/src/index.js
+++ radosnysenior/src/index.js
@@ -1,43 +1,29 @@
-import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
-import {
-  Route,
-  Router,
-  Redirect,
-} from 'react-router';
+import 'normalize.css';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import createSagaMiddleware from 'redux-saga';
 import reducers from './state/reducers';
 import sagas from './state/sagas';
-import Example from './containers/example/example'
+import example from './containers/example/example';
+import login from './components/LoginComponent/LoginComponent';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-  reducers,
-  applyMiddleware(sagaMiddleware)
-)
-
-sagas.forEach(sagaMiddleware.run);
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
 
 sagas.forEach(sagaMiddleware.run);
 
 render(
   <Provider store={store}>
-    <Example />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={login} />
+        <Route path="/xxx" component={example} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
-)
-// render(
-//   <Provider store={store}>
-//     <Router history={history}>
-//       <Route path="/" component={example}>
-//         <Route path="change_password" component={example} />
-//         <Redirect from="*" to="/" />
-//       </Route>
-//     </Router>
-//   </Provider>,
-//   document.getElementById('root')
-// );
+  document.getElementById('root'),
+);
