fd4b40d167d078b5e119773e33486042b6ce4ab8
Jakub Lemka <lemka.jakub@gmail.com>
2019-01-03 20:20:05 +0100

    user details component finished

--- radosnysenior/src/components/UserDetailsComponent/UserDetailsComponent.jsx
+++ radosnysenior/src/components/UserDetailsComponent/UserDetailsComponent.jsx
@@ -5,10 +5,11 @@ import { withFirebase } from '../Firebase';
 import { withRouter } from 'react-router-dom';
 import AppBarComponent from '../AppBarComponent/AppBarComponent';
 import './UserDetailsComponent.css';
-import { Paper } from '@material-ui/core';
+import { Paper, TextField, Select, MenuItem } from '@material-ui/core';
 import LoaderComponent from '../LoaderComponent/LoaderComponent';
 import _ from 'lodash';
-import StarComponent from './StarComponent';
+import StarComponent from '../StarComponent/StarComponent';
+import UserRateComponent from '../UserRateComponent/UserRateComponent';
 
 const PostDetailsPage = () => <PostDetails />;
 
@@ -21,6 +22,9 @@ class PostDetailsComponent extends PureComponent {
       users: [],
       loading: false,
       currentUser: null,
+      isCommentBoxVisible: false,
+      newRateRate: '',
+      newRateRateContent: '',
     };
   }
 
@@ -62,6 +66,18 @@ class PostDetailsComponent extends PureComponent {
     if (unsubscribe) unsubscribe();
   }
 
+  handleNewRate = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  toggleCommentVisible = () => {
+    this.setState(prevState => ({
+      isCommentBoxVisible: !prevState.isCommentBoxVisible,
+    }));
+  };
+
   render() {
     const { currentUser, loading, userRating } = this.state;
     return (
@@ -98,13 +114,63 @@ class PostDetailsComponent extends PureComponent {
                     </a>
                   </h2>
                   {_.times(userRating, i => (
-                    <StarComponent color="#FFDA44" key={i} />
+                    <StarComponent size="48px" color="#FFDA44" key={i} />
                   ))}
                   {_.times(5 - userRating, i => (
-                    <StarComponent color="black" key={i} />
+                    <StarComponent size="48px" color="black" key={i} />
                   ))}
                 </div>
+                <svg
+                  aria-hidden="true"
+                  data-prefix="far"
+                  data-icon="comment"
+                  className="svg-inline--fa fa-comment fa-w-16 commentIcon"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fontSize="15px"
+                  onClick={() => this.toggleCommentVisible()}
+                >
+                  <path d="M256 32C114.6 32 0 125.1 0 240c0 47.6 19.9 91.2 52.9 126.3C38 405.7 7 439.1 6.5 439.5c-6.6 7-8.4 17.2-4.6 26S14.4 480 24 480c61.5 0 110-25.7 139.1-46.3C192 442.8 223.2 448 256 448c141.4 0 256-93.1 256-208S397.4 32 256 32zm0 368c-26.7 0-53.1-4.1-78.4-12.1l-22.7-7.2-19.5 13.8c-14.3 10.1-33.9 21.4-57.5 29 7.3-12.1 14.4-25.7 19.9-40.2l10.6-28.1-20.6-21.8C69.7 314.1 48 282.2 48 240c0-88.2 93.3-160 208-160s208 71.8 208 160-93.3 160-208 160z" />
+                </svg>
               </Paper>
+              {this.state.isCommentBoxVisible && (
+                <div className="newRateForm">
+                  <TextField
+                    id="outlined-multiline-flexible"
+                    label="Opinia o użytkowniku"
+                    multiline
+                    rowsMax="8"
+                    value={this.state.newRateRateContent}
+                    onChange={this.handleNewRate('newRateRateContent')}
+                    margin="normal"
+                    style={{
+                      width: '50%',
+                    }}
+                  />
+                  <Select
+                    value={this.state.newRateRate}
+                    onChange={this.handleNewRate('newRateRate')}
+                    style={{
+                      width: '50%',
+                    }}
+                  >
+                    <MenuItem value="" />
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                  </Select>
+                  <span className="addRateButton" onClick={this.addComment}>
+                    Dodaj ocenę
+                  </span>
+                </div>
+              )}
+              {currentUser.rates &&
+                currentUser.rates.map((rate, index) => (
+                  <UserRateComponent rate={rate} key={index} />
+                ))}
             </React.Fragment>
           )
         )}
