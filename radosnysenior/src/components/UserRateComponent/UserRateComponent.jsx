bc92ace22f429adee9eb9f17a04617bfebb3c944
Jakub Lemka <lemka.jakub@gmail.com>
2019-01-03 20:19:56 +0100

    user rate refactored

--- radosnysenior/src/components/UserRateComponent/UserRateComponent.jsx
+++ radosnysenior/src/components/UserRateComponent/UserRateComponent.jsx
@@ -3,6 +3,8 @@ import * as moment from 'moment';
 import { Link } from 'react-router-dom';
 import './UserRateComponent.css';
 import { Paper } from '@material-ui/core';
+import _ from 'lodash';
+import StarComponent from '../StarComponent/StarComponent';
 
 const UserRateComponent = ({ rate }) => (
   <Paper style={{ borderRadius: '20px', margin: '0 5%' }}>
@@ -15,6 +17,14 @@ const UserRateComponent = ({ rate }) => (
           {rate.addedBy}
         </Link>
         ; {moment(rate.created).format('DD.MM.YYYY, HH:mm')}
+      </div>
+      <div className="rateContent">
+        {_.times(rate.rate, i => (
+          <StarComponent size="24px" color="#FFDA44" key={i} />
+        ))}
+        {_.times(5 - rate.rate, i => (
+          <StarComponent size="24px" color="black" key={i} />
+        ))}
       </div>
       <div className="rateContent">{rate.content}</div>
     </div>
