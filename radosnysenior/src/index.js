8e653001509d3552ff3d9e78eda81ee622a71477
Jakub Lemka <lemka.jakub@gmail.com>
2019-01-03 16:07:44 +0100

    refactor index.js, service worker and app provider

--- radosnysenior/src/index.js
+++ radosnysenior/src/index.js
@@ -1,60 +1,8 @@
-// import React from 'react';
-// import { render } from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { applyMiddleware, createStore, compose } from 'redux';
-// import 'normalize.css';
-import './assets/styles/global.css';
-// import { Route, Switch, BrowserRouter } from 'react-router-dom';
-// import createSagaMiddleware from 'redux-saga';
-// import reducers from './state/reducers';
-// import sagas from './state/sagas';
-// import MainPage from './containers/MainPage/MainPage';
-// import Login from './containers/Login/Login';
-// import PostDetails from './containers/PostDetails/PostDetais';
-// import AddPost from './containers/AddPost/AddPost';
-// import App from './App';
-// import SignIn from './components/auth/SignIn';
-// import SignUp from './components/auth/SignUp';
-// // import { createStore } from 'redux';
-// import rootReducer from './store/reducers/rootReducer';
-// // import rootReducer from './store/reducers/rootReducer';
-// import thunk from 'redux-thunk';
-// import { reduxFirestore, getFirestore } from 'redux-firestore';
-// import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
-// import fbConfig from './config/fbConfig';
-
-// // const sagaMiddleware = createSagaMiddleware();
-
-// //const store = createStore(rootReducer, /*applyMiddleware(sagaMiddleware)*/);
-// // const store = createStore(reducers, applyMiddleware(sagaMiddleware));
-// const store = createStore(rootReducer,
-//     compose(
-//       applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-//       reduxFirestore(fbConfig),
-//       reactReduxFirebase(fbConfig)
-//     )
-//   );
-
-// // sagas.forEach(sagaMiddleware.run);
-
-// render(
-//   <Provider store={store}>
-//     <BrowserRouter>
-//       <Switch>
-//         <Route exact path="/" component={Login} />
-//         <Route path="/signup" component={SignUp} />
-//         <Route path="/main" component={MainPage} />
-//         <Route path="/add" component={AddPost} />
-//         <Route name="posts" exact path="/posts/:id" component={PostDetails} />
-//       </Switch>
-//     </BrowserRouter>
-//   </Provider>,
-//   document.getElementById('root'),
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'normalize.css';
+import './assets/styles/global.css';
 import * as serviceWorker from './serviceWorker';
 import Firebase, { FirebaseContext } from './components/Firebase';
 
