59e140717909202580ae3008c6fd403ec6bcb02e
Jakub Lemka <lemka.jakub@gmail.com>
2019-01-03 15:38:21 +0100

    firebase config file,remove old one

---
+++ radosnysenior/src/components/Firebase/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyCu_gcwP_of1I7PqHuzDbTIP4xcToMPIIg',
+  authDomain: 'radosny-senior.firebaseapp.com',
+  databaseURL: 'https://radosny-senior.firebaseio.com',
+  projectId: 'radosny-senior',
+  storageBucket: 'radosny-senior.appspot.com',
+  messagingSenderId: '707502575274',
+};
+
+export default config;
